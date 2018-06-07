704305dc554a8edf83ac6e3f1afc6a16e4b82f8f
James <james.hw.kim@gmail.com>
2018-06-07 15:04:10 +0800

    final project upload

---
+++ myprofile/js/main2.js
@@ -0,0 +1,15 @@
+(function(){
+  "use strict";
+  /* Start of your code */
+  
+ 	console.log('the script is running')
+
+ 	let headerElement = document.getElementById('header')
+
+ 	console.log(headerElement);
+
+
+
+
+  /* End of your code */
+})();
